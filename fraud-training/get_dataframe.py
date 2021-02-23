fd5795fd12b6d7ae92ddf3c499cd5f2cb3e56d69
jmsaylor <johnmsaylor@gmail.com>
2021-02-23 17:40:50 -0500

    refactoring to understand

--- fraud-training/get_dataframe.py
+++ fraud-training/get_dataframe.py
@@ -4,6 +4,8 @@ import numpy as np
 def data_structure(dataframe, shuffle=True, batch_size=5):
     dataframe = dataframe.copy()
     labels = dataframe.pop('isFlaggedFraud')
-    data_set = tf.data.Dataset.from_tensor_slices(dict(dataframe), labels)
+    data_set = tf.data.Dataset.from_tensor_slices(dict(dataframe))
+
+
     data_set.batch(batch_size)
     return data_set
--- fraud-training/main.py
+++ fraud-training/main.py
@@ -3,23 +3,39 @@ import tensorflow as tf
 import numpy as np
 from get_dataframe import data_structure
 
-filename = '/home/jm/Data/test_PS_20174392719_1491204439457_log.csv'
-data = pandas.read_csv(filename)
-ds = data_structure(data)
-ds.
-print(data.head())
+def collect_data(path):
+    filename = path
+    data_frame = pandas.read_csv(filename)
+    # data_frame = tf.data.experimental.CsvDataset(filename, record_defaults)
+    return data_frame
 
+def prepare_data(data):
+    array = np.array(data)
+    ys = array[:, : -2]
+    xs = array[1:]
 
-# data = np.array(data)
-# data = data[1:]
-# xs, ys = data[:, :-1], data[:, -1]
-# xs = np.asarray(xs).astype(np.float32)
-# ys = np.asarray(ys).astype(np.float32)
-#
-# model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[9,])])
-# model.compile(optimizer='sgd', loss='mean_squared_error')
-#
-# model.summary()
-# model.fit(xs, ys, epochs=500)
-#
+    return xs, ys
+
+    # ys = ys[:, :1]
+    # print(ys)
+
+    # xs = np.asarray(xs).astype(np.float32)
+    # ys = np.asarray(ys).astype(np.float32)
+    # ds = data_structure(data)
+    # print(ds.element_spec)
+
+def build_model(dataframe):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Input(shape=(8,)))
+    model.add(tf.keras.layers.Dense(32, activation="relu"))
+    model.add(tf.keras.layers.Dense(1))
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    # model.summary()
+    return model
+
+def train_model(model, xs, ys):
+    model.fit(xs, ys, epochs=500)
+
+data = collect_data('/home/jm/Data/test_PS_20174392719_1491204439457_log.csv')
+print(data.describe(''))
 # print(model.predict([20]))
