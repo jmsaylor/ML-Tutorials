35ca9b1cfe1f40a468b5e5e1648231f23340db81
jmsaylor <johnmsaylor@gmail.com>
2021-02-24 23:05:35 -0500

    might be getting somewhere

--- fraud-training/main.py
+++ fraud-training/main.py
@@ -1,37 +1,64 @@
 import pandas
 import tensorflow as tf
+tf.executing_eagerly()
+
+from sklearn import preprocessing
 import numpy as np
 from get_dataframe import data_structure
 
+
 def prepare_data(path):
-    filename = path
-    data_frame = pandas.read_csv(
-        filename,
-        header=0,
-        index_col=False,
-        usecols=[1, 2, 3, 4, 5, 6, 7, 8, 9],
-        )
+    df = pandas.read_csv(path)
+    df.pop('step'); df.pop('isFlaggedFraud')
+    df.pop('nameOrig'); df.pop('nameDest')
+    # print(df.head())
+
+    data = {}
+
+    columns = ['type', 'amount', 'nameOrig', 'oldbalanceOrg', 'newbalanceOrig',
+     'nameDest', 'oldbalanceDest', 'newbalanceDest', 'isFraud']
+
+    types = np.array(df.pop('type'))
+
+    # #TODO: improve to one-hot encoding
+    # label_encoder = preprocessing.LabelEncoder().fit(types)
+    # types = label_encoder.transform(types)
+    # data['type'] = np.array(types, dtype=np.int)
+
+    numeric_data_cols = ['amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest']
+
+    numeric_data = []
+
+    for name in numeric_data_cols:
+        numeric_data.append(df.pop(name).values)
+
+    amounts_scaler = preprocessing.MinMaxScaler().fit(numeric_data)
+    scaled_numeric_data = amounts_scaler.transform(numeric_data)
+
+    for index in range(len(numeric_data_cols)):
+        data[numeric_data_cols[index]] = np.array(scaled_numeric_data[index], dtype=float)
+
+    target = []
+    isFraud = df.pop('isFraud').values
+    for i in range(len(isFraud)):
+        target.append([1, 1, 1, 1, isFraud[i]])
+        # target.append([isFraud[i]])
+
+    target = np.array(target)
+
+    source_data = []
+    for name in data:
+        source_data.append(data[name])
 
 
-    data_frame['type'] = pandas.Categorical(data_frame['type'])
+    source_data = np.array(source_data)
+    source_data = source_data.transpose()
+    return source_data, target
 
-    print(data_frame.dtypes)
-
-    labels = data_frame.pop('isFraud')
-    features = data_frame
-
-    data_set = tf.data.Dataset.from_tensor_slices((features.values, labels.values))
-    # print(data_set.element_spec)
-
-
-
-    # ds = data_structure(data)
-    # print(ds.element_spec)
 
 def build_model():
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Input(shape=(8,)))
-    model.add(tf.keras.layers.Dense(32, activation="relu"))
+    model.add(tf.keras.layers.Input(shape=(None , 5)))
     model.add(tf.keras.layers.Dense(1))
     model.compile(optimizer='sgd', loss='mean_squared_error')
     model.summary()
@@ -40,7 +67,8 @@ def build_model():
 def train_model(model, xs, ys):
     model.fit(xs, ys, epochs=500)
 
-pandas.set_option("display.max_columns", 11)
-prepare_data('/home/jm/Data/test_PS_20174392719_1491204439457_log.csv')
-# prepare_dataset(data)
-# print(model.predict([20]))
+xs, ys = prepare_data('/home/jm/Data/test_PS_20174392719_1491204439457_log.csv')
+print(xs)
+model = build_model()
+model.fit(xs, ys, epochs=5)
+print(model.predict([[1,0,1,0,1]]))
